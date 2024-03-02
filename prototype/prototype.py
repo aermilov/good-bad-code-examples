c65883c1e3c18f554dafaaa31c4a71218507b00d
YaArtem333 <aermilov2003@gmail.com>
2024-03-02 10:02:19 +0300

    good code

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -1,4 +1,11 @@
-class Dish:
+import copy
+
+
+class DishPrototype:
+    def clone(self):
+        return copy.deepcopy(self)
+
+class Dish(DishPrototype):
     def __init__(self, name, ingredients, price):
         self.name = name
         self.ingredients = ingredients
@@ -12,7 +19,12 @@ class Order:
 class RestaurantManagementSystem:
     def __init__(self):
         self.orders = []
+        self.dish_prototypes = {}
+
+    def add_dish_prototype(self, dish):
+        self.dish_prototypes[dish.name] = dish
 
     def create_order(self, dishes, table_number):
+        dishes = [self.dish_prototypes[name].clone() for name in dish_names]
         order = Order(dishes, table_number)
         self.orders.append(order)
