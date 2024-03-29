df616ac713d663c8f2335993c5019653780f60d6
YaArtem333 <aermilov2003@gmail.com>
2024-03-29 22:48:23 +0300

    good code

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -1,33 +1,30 @@
 from flask import Flask, request, jsonify
+from functools import wraps
+
 
 app = Flask(__name__)
 
-@app.route('/public')
-def public_endpoint():
-    print(f"Запрос к {request.path} от {request.remote_addr}")
+def log_access(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print(f"Запрос к {request.path} от {request.remote_addr}")
+        return f(*args, **kwargs)
+    return decorated_function
 
+def authenticate(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        auth_token = request.headers.get('Authorization')
+        if auth_token != 'MySecretToken':
+            return jsonify(message="Ошибка аутентификации."), 403
+        return f(*args, **kwargs)
+    return decorated_function
+
+def public_endpoint():
     return jsonify(message="Это публичный эндпоинт, доступный всем.")
+app.route('/public')(log_access(public_endpoint))
 
 @app.route('/secret')
 def secret_endpoint():
-    print(f"Запрос к {request.path} от {request.remote_addr}")
-
-    auth_token = request.headers.get('Authorization')
-    if auth_token != 'MySecretToken':
-        return jsonify(message="Ошибка аутентификации."), 403
     return jsonify(message="Это секретный эндпоинт, доступный только авторизованным пользователям.")
-
-@app.route('/public_decorated')
-def public_decorated_endpoint():
-    print(f"Запрос к {request.path} от {request.remote_addr}")
-
-    return jsonify(message="Этот публичный эндпоинт логируется.")
-
-@app.route('/secret_decorated')
-def secret_decorated_endpoint():
-    print(f"Запрос к {request.path} от {request.remote_addr}")
-
-    auth_token = request.headers.get('Authorization')
-    if auth_token != 'СекретныйТокен':
-        return jsonify(message="Ошибка аутентификации."), 403
-    return jsonify(message="Этот секретный эндпоинт требует аутентификации и его вызовы логируются.")
+app.route('/secret')(log_access(authenticate(secret_endpoint)))
