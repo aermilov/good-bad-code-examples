6216a611ade773b506e435d022d0bdd8d1490e82
YaArtem333 <aermilov2003@gmail.com>
2024-03-29 22:19:57 +0300

    bad code

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -1,42 +1,33 @@
-from functools import wraps
 from flask import Flask, request, jsonify
-
 
 app = Flask(__name__)
 
 @app.route('/public')
 def public_endpoint():
+    print(f"Запрос к {request.path} от {request.remote_addr}")
+
     return jsonify(message="Это публичный эндпоинт, доступный всем.")
 
 @app.route('/secret')
 def secret_endpoint():
+    print(f"Запрос к {request.path} от {request.remote_addr}")
+
+    auth_token = request.headers.get('Authorization')
+    if auth_token != 'MySecretToken':
+        return jsonify(message="Ошибка аутентификации."), 403
     return jsonify(message="Это секретный эндпоинт, доступный только авторизованным пользователям.")
 
-
-def log_access(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        print(f"Запрос к {request.path} от {request.remote_addr}")
-        return f(*args, **kwargs)
-    return decorated_function
-
-def authenticate(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        auth_token = request.headers.get('Authorization')
-        if auth_token != 'MySecretToken':
-            return jsonify(message="Ошибка аутентификации."), 403
-        return f(*args, **kwargs)
-    return decorated_function
-
-
 @app.route('/public_decorated')
-@log_access
 def public_decorated_endpoint():
+    print(f"Запрос к {request.path} от {request.remote_addr}")
+
     return jsonify(message="Этот публичный эндпоинт логируется.")
 
 @app.route('/secret_decorated')
-@log_access
-@authenticate
 def secret_decorated_endpoint():
+    print(f"Запрос к {request.path} от {request.remote_addr}")
+
+    auth_token = request.headers.get('Authorization')
+    if auth_token != 'СекретныйТокен':
+        return jsonify(message="Ошибка аутентификации."), 403
     return jsonify(message="Этот секретный эндпоинт требует аутентификации и его вызовы логируются.")
